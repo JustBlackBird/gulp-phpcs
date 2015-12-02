4097dd68a4f4fdcadda1c5b0760a906ef0cbd695
Dmitriy Simushev <simushevds@gmail.com>
2015-12-02 10:23:45 +0000

    Fix all Linux CLI fixtures

--- test/fixture/error.sh
+++ test/fixture/error.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Wait until stdin is closed.
+cat > /dev/null
 # It's just an arbitrary text.
 echo "This is a test error."
 # PHPCS uses exit-codes greater than 1 to report about real errors.
--- test/fixture/zero.sh
+++ test/fixture/zero.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Wait until stdin is closed.
+cat > /dev/null
 # The script exits with zero exit code. The message it sends to stdout has
 # no meaning.
 echo "This script ends up with zero exit-code."
