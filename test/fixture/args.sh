f001f0e99000d0b3f2965d7eb034197e2bc918ba
Dmitriy Simushev <simushevds@gmail.com>
2015-12-02 10:16:29 +0000

    Fix CLI fixture for args builder

--- test/fixture/args.sh
+++ test/fixture/args.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Wait until stdin is closed.
+cat > /dev/null
 # Just output all arguments as string to stdout.
 echo $*
 # Pretend that passed in args are report about codding style problems.
